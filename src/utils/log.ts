3d26a6e70ddcbbb295e0074001d0791946230b5b
x3noku <x3noku@gmail.com>
2022-09-18 17:04:45 +0300

    Update Logger Config commit

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,13 +1,24 @@
-import { logger } from 'xenous-logs';
+import { consoleTransport, logger } from 'xenous-logs';
 
 const config = {
+    transport: consoleTransport,
+    severity: 'debug',
     levels: {
         info: 6,
         error: 3,
+        // dev levels
         warn: 2,
         debug: 1,
     },
+    transportOptions: {
+        color: 'ansi',
+    },
     enabledExtensions: ['command'] as const,
+    async: true,
+    dateFormat: 'time',
+    printLevel: true,
+    printDate: true,
+    enabled: true,
 };
 
 const log = logger.createLogger<typeof config.levels, typeof config.enabledExtensions[number]>(config);
