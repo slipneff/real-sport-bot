3c8066950c4647423e68d4e3ff0831e3a20ecd53
x3noku <x3noku@gmail.com>
2022-09-20 19:42:51 +0300

    Update Strings commit

--- src/scenes/invalid.ts
+++ src/scenes/invalid.ts
@@ -6,10 +6,6 @@ import keyboard from '@utils/keyboard';
 const scene = new Scene(Scenes.INVALID);
 
 // todo: add string for invalid state
-scene.enter(async ctx => await ctx.reply(strings.invalid.greeting, keyboard([[{ text: strings.invalid.start }]])));
-scene.hears(strings.invalid.start, async ctx => {
-    await ctx.reply('/start', keyboard());
-    await ctx.scene.leave();
-});
+scene.enter(async ctx => await ctx.reply(strings.invalid.greeting, keyboard()));
 
 export default scene;
--- src/scenes/results.ts
+++ src/scenes/results.ts
@@ -1,10 +1,13 @@
 import { Scenes } from '@utils/constants';
 import strings from '@utils/strings';
 import Scene from 'telegraf/scenes/base';
-
-const init = async ctx => await ctx.reply(strings.quiz.score(ctx.session.state.participant.score));
+import keyboard from '@utils/keyboard';
 
 const scene = new Scene(Scenes.RESULTS);
-scene.enter(init);
+scene.enter(async ctx => {
+    await ctx.reply(strings.results.greeting, keyboard());
+    await ctx.reply(strings.results.vk, keyboard());
+    await ctx.reply(strings.results.telegram, keyboard());
+});
 
 export default scene;
