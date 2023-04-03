b0443131362b04b3243eff8e5a019dbdc639d7d9
slipneff <88764276+slipneff@users.noreply.github.com>
2023-04-04 00:04:29 +0300

    x

--- src/scenes/training2_hard.ts
+++ src/scenes/training2_hard.ts
@@ -17,6 +17,12 @@ scene.enter(async ctx => {
             ],
         ]),
     );
+    await ctx.replyWithAnimation('CgACAgIAAxkBAAEfNrpkKz1bIJSSZbLsmvk3P_4bv_GZXAAC2isAAlhgWEk46VkIbZiW-C8E');
+    await ctx.replyWithAnimation('CgACAgIAAxkBAAEfNpVkKzuSuf-bZLO1bTWR1OTy2Hv5PAACzCsAAlhgWElxWrxcdOAV5S8E');
+    await ctx.replyWithAnimation('CgACAgIAAxkBAAEfNpZkKzuSvHG9TdE3k7S8fCXaHAzW2AACzisAAlhgWEk4LTP4qruQCy8E');
+    await ctx.replyWithAnimation('CgACAgIAAxkBAAEfNpdkKzuSA2k22CMsavwfYshi-kE5vwACzysAAlhgWEl4CD2Pj06-mS8E');
+    await ctx.replyWithAnimation('CgACAgIAAxkBAAEfNphkKzuS5z1CcoVqKTT0FSi2oDZ51QAC0CsAAlhgWElIW3QjLYRCzC8E');
+    await ctx.replyWithAnimation('CgACAgIAAxkBAAEfNplkKzuSor4hVSocl36yAh0SsRnq3QAC0SsAAlhgWEm5__bAon7oxy8E');
 });
 scene.hears(strings.menu, async ctx => await ctx.scene.enter(Scenes.TRAINING2));
 
--- src/scenes/training4_medium.ts
+++ src/scenes/training4_medium.ts
@@ -17,6 +17,12 @@ scene.enter(async ctx => {
             ],
         ]),
     );
+    await ctx.replyWithAnimation('CgACAgIAAxkBAAEfNslkKz5ZYCfkWNdLukql7v16BQKuFAAC2ysAAlhgWElLwnj3f3WrDS8E');
+    await ctx.replyWithAnimation('CgACAgIAAxkBAAEfNspkKz5ZQ_vL9kM8MzDLqf9_X6W9IgAC3CsAAlhgWEm5lNuCnjTsvC8E');
+    await ctx.replyWithAnimation('CgACAgIAAxkBAAEfNstkKz5ZgZRPJ4cWvE2NMLUAAefIFg0AAt0rAAJYYFhJd2Z_fQvGn_wvBA');
+    await ctx.replyWithAnimation('CgACAgIAAxkBAAEfNsxkKz5ZnRnxghWfENbdlrt3gtD5ZAAC3isAAlhgWEn8XjNZJtHnZC8E');
+    await ctx.replyWithAnimation('CgACAgIAAxkBAAEfNs5kKz5ZHnkjut9rJbCNenZeAtDM3QAC4CsAAlhgWElj6hOCj-prHi8E');
+    await ctx.replyWithAnimation('CgACAgIAAxkBAAEfNs1kKz5Zupc__w3_xSS6U_rBZbF80QAC3ysAAlhgWEm6X2O4lNxM2i8E');
 });
 scene.hears(strings.menu, async ctx => await ctx.scene.enter(Scenes.TRAINING4));
 
