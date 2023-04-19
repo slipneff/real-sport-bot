a86b972afbb7bd4732b460ac8b95e3ea6750afb1
slipneff <88764276+slipneff@users.noreply.github.com>
2023-04-20 01:54:46 +0300

    x

--- src/scenes/week3.ts
+++ src/scenes/week3.ts
@@ -22,11 +22,11 @@ scene.enter(async ctx => {
         ]),
     );
 });
-scene.hears(strings.trainings.training6.name, async ctx => await ctx.scene.enter(Scenes.TRAINING11));
-scene.hears(strings.trainings.training7.name, async ctx => await ctx.scene.enter(Scenes.TRAINING12));
-scene.hears(strings.trainings.training8.name, async ctx => await ctx.scene.enter(Scenes.TRAINING13));
-scene.hears(strings.trainings.training9.name, async ctx => await ctx.scene.enter(Scenes.TRAINING14));
-scene.hears(strings.trainings.training10.name, async ctx => await ctx.scene.enter(Scenes.TRAINING15));
+scene.hears(strings.trainings.training11.name, async ctx => await ctx.scene.enter(Scenes.TRAINING11));
+scene.hears(strings.trainings.training12.name, async ctx => await ctx.scene.enter(Scenes.TRAINING12));
+scene.hears(strings.trainings.training13.name, async ctx => await ctx.scene.enter(Scenes.TRAINING13));
+scene.hears(strings.trainings.training14.name, async ctx => await ctx.scene.enter(Scenes.TRAINING14));
+scene.hears(strings.trainings.training15.name, async ctx => await ctx.scene.enter(Scenes.TRAINING15));
 scene.hears(strings.menu, async ctx => await ctx.scene.enter(Scenes.WEEKS));
 
 export default scene;
